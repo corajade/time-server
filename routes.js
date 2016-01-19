989eebeb9cf9b486da02a0396e7b6c98a17f1471
Kristen <corajade@gmail.com>
2016-01-19 17:55:24 +0000

    routes updated, string check function added

--- routes.js
+++ routes.js
@@ -1,12 +1,16 @@
 var express=require("express");
 var app=express();
 var url=require("url");
+var checkStr=require(process.cwd()+ "/checkStr.js");
 
 app.get(/./, function(req, res){
   var obj= url.parse(req.url); 
-  console.log(obj);
-  console.log(obj["path"]);
-  res.end("Done")
+   
+  var str=obj["path"].slice(1);
+  var answer=checkStr(str);
+
+  res.send(answer);
+  res.end();
 });
 
 app.listen(8080);
